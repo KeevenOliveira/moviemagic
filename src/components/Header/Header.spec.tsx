f48640c632d8833d85379fb7b5129556dc95878d
Keeven Oliveira <keevinhuholiveira@gmail.com>
2024-08-04 17:53:05 -0300

    feat: add header component and tests

---
+++ src/components/Header/Header.spec.tsx
@@ -0,0 +1,46 @@
+import { render, screen } from "@/test/test-utils";
+import { usePathname } from "next/navigation";
+
+import Header from ".";
+
+jest.mock("next/navigation", () => ({
+  usePathname: jest.fn(),
+}));
+
+describe("<Header/>", () => {
+  it("should render Header", () => {
+    render(<Header />);
+
+    const text = screen.getByText("Movie Magic");
+    const logo = screen.getByTestId("logo");
+
+    expect(logo).toBeVisible();
+    expect(text).toBeInTheDocument();
+  });
+
+  it("should render ChangeHeaderPage", () => {
+    render(<Header />);
+
+    const searchIcon = screen.getByTestId("search-icon");
+    const linkToSearch = screen.getByTestId("to-search");
+
+    expect(searchIcon).toBeVisible();
+    expect(linkToSearch).toBeVisible();
+  });
+
+  it("should be able render home icon", () => {
+    (usePathname as jest.Mock).mockReturnValueOnce("/search");
+
+    render(<Header />);
+
+    const homeIcon = screen.getByTestId("home-icon");
+    const linkToHome = screen.getByTestId("to-home");
+    const text = screen.getByText("Movie Magic");
+
+    expect(homeIcon).toHaveProperty("tagName", "svg");
+    expect(homeIcon).toHaveClass("text-3xl text-white-500");
+    expect(text).toBeVisible();
+    expect(homeIcon).toBeInTheDocument();
+    expect(linkToHome).toBeVisible();
+  });
+});
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,27 @@
-import Image from "next/image";
+"use client";
+
+import { usePathname } from "next/navigation";
+import Link from "next/link";
+import { useMemo } from "react";
+import { MdMovieFilter } from "react-icons/md";
+
+import ChangeHeaderPage from "./ActiveLink";
 
 const Header = () => {
+  const pathName = usePathname();
+
+  const isSearch = useMemo(() => {
+    return pathName === "/search";
+  }, [pathName]);
+
   return (
-    <header className="flex ml-1">
-      <h4>Movie Magic</h4>
+    <header className="flex justify-between pt-2 p-3">
+      <Link href="/" className="flex items-center" data-testid="logo">
+        <MdMovieFilter className="text-2xl text-blue-400 mr-2" />
+        <h4>Movie Magic</h4>
+      </Link>
+
+      <ChangeHeaderPage isSearch={isSearch} />
     </header>
   );
 };
