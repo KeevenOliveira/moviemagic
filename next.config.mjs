b69509dcb96b95641d8d3059afda0878bd219380
Keeven Oliveira <keevinhuholiveira@gmail.com>
2024-08-04 21:01:32 -0300

    feat: add validation of domains to access images

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ["media.themoviedb.org"],
+    remotePatterns: [
+      {
+        port: "",
+        hostname: "media.themoviedb.org",
+        pathname: "/t/p/w220_and_h330_face",
+        protocol: "https",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
